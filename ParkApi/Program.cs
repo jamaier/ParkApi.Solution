3d3a5a4db96c2ca895c5055fe62f6f5a9f10a4ab
Jacob Maier <jacobamaier@gmail.com>
2023-03-31 13:29:36 -0700

    WIP CORS added but not sure if its working properly. Still testing.

--- ParkApi/Program.cs
+++ ParkApi/Program.cs
@@ -6,21 +6,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ParkApiContext>(
-                  dbContextOptions => dbContextOptions
-                    .UseMySql(
-                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
-                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                    )
-                  )
-                );
-
+    dbContextOptions => dbContextOptions
+        .UseMySql(
+            builder.Configuration["ConnectionStrings:DefaultConnection"], 
+            ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+        )
+    )
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add CORS
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-// Updated code below!
+
+app.UseCors(builder =>
+{
+    builder.WithOrigins("http://localhost:5001")
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
