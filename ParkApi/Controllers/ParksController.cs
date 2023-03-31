2f6f5939354ab5e42a0447be71cf8803a79c8653
Jacob Maier <jacobamaier@gmail.com>
2023-03-31 09:03:45 -0700

    Update CRUD for controller with tests in swagger.

---
+++ ParkApi/Controllers/ParksController.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkApi.Models;
+
+namespace ParkApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : Controller
+  {
+    private readonly ParkApiContext _db;
+
+    public ParksController(ParkApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+
+    // GET api/parks/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      return park;
+    }
+
+    // POST api/parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+    }
+
+    // PUT api/parks/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Parks.Update(park);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(p => p.ParkId == id);
+    }
+
+    // DELETE api/parks/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
